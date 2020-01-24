c865a69074724fa33f59e2955ae224830a9704a7
Rubi Renteria <rubirenteria43@gmail.com>
2020-01-23 22:33:04 -0700

    creating classes to be used for js. creating calculator function

---
+++ script.js
@@ -0,0 +1,32 @@
+class Calculator{
+    constructor(previousOperandandTextElement, currentOperandandTextElement){
+        this.previousOperandandTextElement = previousOperandandTextElement
+        this.currentOperandandTextElement = currentOperandandTextElement
+
+    }
+    clear(){
+
+    }
+    delete(){
+
+    }
+    appendNumber(){
+
+    }
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+    updateDisplay(){
+        
+    }
+}
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const deleteButton = document.querySelector('[data-delete')
+const equalButton = document.querySelector('[data-equals')
+const clearAllButtons = document.querySelectorAll('[data-all-clear')
+const previousOperandandTextElement = document.querySelectorAll('[data-previous-operand')
+const currentOperandandTextElement = document.querySelectorAll('[data-current-operand')
